fc3854bd0fdab4293eab22480d78a4479f03466c
Declan20-dbd <declananthony176@gmail.com>
2025-04-23 15:40:22 +0200

    code int number
    5

---
+++ repo test/Assets/scripts/scriptalpha.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class 123 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int number = 5;
+        Debug.Log(number)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
